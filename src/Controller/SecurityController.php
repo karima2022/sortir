5803111828108ff6190d4ca9ec71ea5014f7a9c7
Johann-B <job.code@outlook.fr>
2023-01-25 11:52:00 +0100

    Modif SecurityController.php

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -31,10 +31,8 @@ class SecurityController extends AbstractController
      */
     public function logout():Response
     {
-        return $this->render('security/login.html.twig');{
-
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
-    }}
+    }
 
     //public function logout(AuthenticationUtils $authenticationUtils): Response
     //{
